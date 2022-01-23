5bc55ee57d5a5275297e50bbc9d55128b499d0db
masonmol <morganlmason96@gmail.com>
2022-01-22 19:05:38 -0500

    Created basic CSS and added default word

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header"></header>
+        <header className="App-header text-center">dictionary.</header>
         <main>
-          <Search />
+          <Search defaultKeyword="sunrise" />
         </main>
         <footer className="footer text-center">
           Coded by{" "}
--- src/Search.js
+++ src/Search.js
@@ -3,31 +3,49 @@ import axios from "axios";
 import Results from "./Results.js";
 import "./Search.css";
 
-export default function Search() {
-  let [keyword, setKeyword] = useState("");
+export default function Search(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
-  function handleKeywordChange(event) {
-    setKeyword(event.target.value);
-  }
-  function search(event) {
-    event.preventDefault();
-
+  function search() {
     // documentation: http://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
 
-  return (
-    <div className="Search">
-      <form onSubmit={search}>
-        <input type="search" onChange={handleKeywordChange} />
-      </form>
-      <Results results={results} />
-    </div>
-  );
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Search">
+        <section>
+          <form onSubmit={handleSubmit}>
+            <input type="search" onChange={handleKeywordChange} />
+          </form>
+          <div className="hint text-center">
+            suggested words: sunset, yoga, manifest, joy...
+          </div>
+        </section>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return "loading";
+  }
 }
